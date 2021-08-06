8a0a721cd6011b10f6e4ddb5df572f6a82d1d27e
senpai-10 <bmjfdrh@gmail.com>
2021-08-06 17:22:23 +0300

    check if input is empty

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
 
-function calculate() {
+function Calculate() {
+  let episodes_number_input = document.getElementById("number-of-episodes")
   let episodes_number = document.getElementById("number-of-episodes").value
-  console.log(episodes_number)
+  let calculate = document.getElementById("calculate-button")
+
+  
+  if (!episodes_number) {
+    episodes_number_input.style.borderColor = "#a94646"
+    calculate.style.backgroundColor = "#a94646"
+    return
+  }
 }
 
 function App() {
   return (
     <div className="App">
-      <input id="number-of-episodes" placeholder="Number of episodes"/>
-      <button id="calculate-button" onClick={calculate}>Calculate</button>
+      <input 
+        id="number-of-episodes" 
+        placeholder="Number of episodes"
+        onKeyPress={(event) => {
+          if (!/[0-9]/.test(event.key)) {
+            event.preventDefault();
+          }
+        }}
+      />
+      <button 
+        id="calculate-button" 
+        onClick={Calculate}>
+          Calculate
+      </button>
 
       <p>
       </p>
