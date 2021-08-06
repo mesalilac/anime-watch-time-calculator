f285583d1d8bce905f74c5fd2fba0bfbbb35e9c1
senpai-10 <bmjfdrh@gmail.com>
2021-08-06 17:35:30 +0300

    check if input value is not a number

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 
 function Calculate() {
+  const colors = {
+    errorBorderColor: "#a94646",
+    errorBackGroundColor: "#a94646",
+    defaultBorderColor: "#4654a9",
+    defaultBackGroundColor: "#4654a9"
+  }
+  
   let episodes_number_input = document.getElementById("number-of-episodes")
   let episodes_number = document.getElementById("number-of-episodes").value
   let calculate = document.getElementById("calculate-button")
-
   
-  if (!episodes_number) {
-    episodes_number_input.style.borderColor = "#a94646"
-    calculate.style.backgroundColor = "#a94646"
+  if (!episodes_number || !Number(episodes_number)) {
+    episodes_number_input.style.borderColor = colors.errorBorderColor
+    calculate.style.backgroundColor = colors.errorBackGroundColor
     return
+  } else {
+    episodes_number_input.style.borderColor = colors.defaultBorderColor
+    calculate.style.backgroundColor = colors.defaultBackGroundColor
+    
   }
 }
 
