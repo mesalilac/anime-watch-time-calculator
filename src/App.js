dc13909fa71d47a3ae5192a9e9dfd50d93c3aa58
senpai-10 <bmjfdrh@gmail.com>
2021-08-06 16:38:38 +0300

    calculate funtions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+function calculate() {
+  let episodes_number = document.getElementById("number-of-episodes").value
+  console.log(episodes_number)
+}
 
 function App() {
   return (
     <div className="App">
       <input id="number-of-episodes" placeholder="Number of episodes"/>
-      <button id="calculate-button">Calculate</button>
+      <button id="calculate-button" onClick={calculate}>Calculate</button>
 
       <p>
       </p>
